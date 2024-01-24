2c3c489b8500cc217512960d1dadcce5cdf57e88
CooperPurvis <cooperpurvis@gmail.com>
2024-01-24 13:59:43 -0600

    Initial commit

---
+++ src/main/java/com/example/CommerceBank/CommerceBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.CommerceBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CommerceBank/CommerceBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommerceBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
