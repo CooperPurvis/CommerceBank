47a2e612f0f1be5a0746956abce3d6ca2be2e0b8
Blake Ricketson <blakericketson@yahoo.com>
2024-03-25 16:43:04 -0500

    Updated Entity Classes on the Backend

--- src/main/java/org/example/commercebank/domain/AppInfo.java
+++ src/main/java/org/example/commercebank/domain/AppInfo.java
@@ -9,13 +9,13 @@ import lombok.AllArgsConstructor;
 @Data
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 public class AppInfo {
-
 
     @Id
     private String app_info_uid;
     private String app_info_description;
 
-
+    AppInfo(String app_info_description) {
+        this.app_info_description = app_info_description;
+    }
 }
